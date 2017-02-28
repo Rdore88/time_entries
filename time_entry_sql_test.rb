3e01b84393367686c458606dbbc92f11f62d41f3
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2017-02-28 15:05:25 -0500

    Added test to make find the last developer to join and another test to find the number of projects for each client.

--- time_entry_sql_test.rb
+++ time_entry_sql_test.rb
@@ -12,8 +12,18 @@ class SqlTest < Minitest::Test
   end
 
   def test_find_all_time_entries
-  sql = "SELECT * FROM time_entries"
-  assert_equal 500, db.execute(sql).length
+    sql = "SELECT * FROM time_entries"
+    assert_equal 500, db.execute(sql).length
+  end
+
+  def test_find_the_developer_who_joined_most_recently
+    sql = "SELECT name, joined_on FROM developers ORDER BY joined_on DESC LIMIT 1"
+    assert_equal ["Dr. Danielle McLaughlin", "2015-07-10"], db.execute(sql).first
+  end
+
+  def test_find_the_number_of_projects_for_each_client
+    sql = "SELECT clients.name, client_id, COUNT(*) FROM projects INNER JOIN clients WHERE client_id = clients.id GROUP BY client_id"
+    assert_equal ["West Group", 1, 3], db.execute(sql).first
   end
 
 end
